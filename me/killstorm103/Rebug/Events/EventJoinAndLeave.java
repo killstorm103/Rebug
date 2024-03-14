8213aa6250b76276fa5d80a18e5a9d2d1f950ce6
killstorm <38782331+killstorm103@users.noreply.github.com>
2024-03-14 21:25:06 +0800

    Added join message from config

--- me/killstorm103/Rebug/Events/EventJoinAndLeave.java
+++ me/killstorm103/Rebug/Events/EventJoinAndLeave.java
@@ -1,9 +1,15 @@
 package me.killstorm103.Rebug.Events;
 
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.*;
+
+import me.killstorm103.Rebug.Main.Rebug;
+
+
 
 public class EventJoinAndLeave implements Listener
 {
@@ -12,6 +18,10 @@ public class EventJoinAndLeave implements Listener
 	{
 		if (!e.getPlayer().isOp())
 			e.getPlayer().setGameMode(GameMode.SURVIVAL);
+		
+		String message = Rebug.getGetMain().getConfig().getString("join-message");
+		if (message != null && message.length() > 0 && e.getPlayer().isOp())
+			e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', message));
 	}
 	
 	@EventHandler(priority = EventPriority.MONITOR)
