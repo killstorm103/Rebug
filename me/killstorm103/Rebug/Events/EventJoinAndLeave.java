f46681be53e8d0f8c7a6f67f83d005d7c59db892
killstorm <38782331+killstorm103@users.noreply.github.com>
2024-03-14 17:26:15 +0800

    Added Force GameMode s

--- me/killstorm103/Rebug/Events/EventJoinAndLeave.java
+++ me/killstorm103/Rebug/Events/EventJoinAndLeave.java
@@ -8,7 +8,11 @@ import org.bukkit.event.player.*;
 public class EventJoinAndLeave implements Listener
 {
 	@EventHandler(priority = EventPriority.LOWEST)
-	public void onJoin (PlayerJoinEvent e) {}
+	public void onJoin (PlayerJoinEvent e) 
+	{
+		if (!e.getPlayer().isOp())
+			e.getPlayer().setGameMode(GameMode.SURVIVAL);
+	}
 	
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onQuit (PlayerQuitEvent e) {}
