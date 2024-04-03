a7965e04f0e0f48e9f0ee03bca0c131bd1ea6dc0
killstorm <38782331+killstorm103@users.noreply.github.com>
2024-04-04 01:19:05 +0800

    Update EventJoinAndLeave.java

--- me/killstorm103/Rebug/Events/EventJoinAndLeave.java
+++ me/killstorm103/Rebug/Events/EventJoinAndLeave.java
@@ -7,63 +7,95 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.*;
-import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Score;
-import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
 import me.killstorm103.Rebug.Main.Rebug;
 import me.killstorm103.Rebug.Utils.PT;
+import me.killstorm103.Rebug.Utils.User;
 
 
 
 public class EventJoinAndLeave implements Listener
 {
-	private Score score = null;
-	
+	public static class BrandListener implements PluginMessageListener
+	{
+	    public void onPluginMessageReceived(String channel, Player player, byte[] msg) 
+	    {
+	    	final User user = Rebug.getUser(player);
+	    	if (user == null)
+	    	{
+	    		player.kickPlayer("Rebug's \"User\" was somehow null, when reading your client brand!");
+	    		return;
+	    	}
+	    	if (user.getIsBrandSet())
+	    	{
+	    		player.kickPlayer("Rebug's already collected your client brand you shound't be sending another client brand Packet!");
+	    		return;
+	    	}
+	    	try
+	    	{
+	    		final String magic = new String(msg, "UTF-8").substring(1);
+				if (!user.getIsBrandSet())
+	    		{
+					user.setBrand(magic);
+					user.setBrandSet(true);
+	    		}
+	    	}
+	    	catch (Exception e) 
+	    	{
+	    		if (user.getBrand() == null || !user.getIsBrandSet() || user.getBrand().length() <= 0)
+	    			player.kickPlayer("Rebug kicked you due to failing to load your client brand!");
+	    		
+	    		e.printStackTrace();
+			}
+	    	
+	    	if (user.getBrand() == null || !user.getIsBrandSet() || user.getBrand().length() <= 0)
+    			player.kickPlayer("Rebug kicked you due to failing to load your client brand!");
+	    }
+	}
+		
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onJoin (PlayerJoinEvent e) 
 	{
 		Player player = e.getPlayer();
+		PT.addChannel(player, "MC|Brand");
+		Rebug.USERS.put(player.getUniqueId(), new User(player));
+		Bukkit.getScheduler().scheduleSyncDelayedTask(Rebug.getGetMain(), new Runnable()
+		{
+			
+			@Override
+			public void run() 
+			{
+				Bukkit.dispatchCommand(Rebug.getGetMain().getServer().getConsoleSender(), "rebug client " + player.getName());
+			}
+			
+		}, 10 * 50);
 		
-		if (Rebug.getGetMain().getConfig().getBoolean("force-gamemode-on-join") && !e.getPlayer().isOp())
+		if (Rebug.getGetMain().getConfig().getBoolean("force-gamemode-on-join") && !player.isOp())
 			player.setGameMode(GameMode.SURVIVAL);
 		
-		String message = Rebug.getGetMain().getConfig().getString("join-message");
-		if (message != null && message.length() > 0 && e.getPlayer().isOp())
-			player.sendMessage(ChatColor.translateAlternateColorCodes('&', message.replace("%player%", e.getPlayer().getName())));
-		
-		
-		if (Rebug.getGetMain().getConfig().getBoolean("rebug-scoreboard"))
-		{
-			score = null;
-			Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(Rebug.getGetMain(), new Runnable() {
-	            public void run() {        
-	            	
-	                ScoreboardManager manager = Bukkit.getScoreboardManager();
-	                final Scoreboard board = manager.getNewScoreboard();
-	                final Objective objective = board.registerNewObjective("Rebugged", "Cummy");        
-	                objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-	                String name = ChatColor.DARK_GRAY + "| " + Rebug.getGetMain().getConfig().getString("scoreboard-title") + " " + ChatColor.DARK_GRAY + "|";
-	                name = ChatColor.translateAlternateColorCodes('&', name);
-	                objective.setDisplayName(name);
-	                score = objective.getScore(ChatColor.DARK_GRAY + "| §cTest §2Server " + ChatColor.DARK_GRAY + "|");
-	                score.setScore(10);       
-	                score = objective.getScore("");
-	                score.setScore(9);    
-	                score = objective.getScore(ChatColor.DARK_GRAY + "| " + ChatColor.DARK_RED + Rebug.PluginName() + ChatColor.DARK_GRAY + " |" + ChatColor.GRAY + " v" + Rebug.PluginVersion());
-	                score.setScore(8);
-	                score = objective.getScore(ChatColor.DARK_GRAY + "| " + ChatColor.DARK_RED + "Server" + ChatColor.DARK_GRAY + " |" + ChatColor.GRAY + " " + PT.getServerVersion ());
-	                score.setScore(7);
-	                score = objective.getScore(ChatColor.DARK_GRAY + "| " + ChatColor.DARK_RED + "Client" + ChatColor.DARK_GRAY + " | " + ChatColor.GRAY + PT.getPlayerVersion(PT.getPlayerVersion(player)));
-	                score.setScore(6);
-	                player.setScoreboard(board);
-	            }
-	        },0, 20 * 10);
-		}
-	}
+		final String message = Rebug.getGetMain().getConfig().getString("join-message");
+		if (message != null && message.length() > 0 && player.isOp())
+			player.sendMessage(ChatColor.translateAlternateColorCodes('&', message.replace("%player%", player.getName())));
+	} 
 	
 	@EventHandler(priority = EventPriority.MONITOR)
-	public void onQuit (PlayerQuitEvent e) {}
+	public void onQuit (PlayerQuitEvent e) 
+	{
+		Player player = e.getPlayer();
+		User user = Rebug.getUser(player);
+		if (user != null)
+			Rebug.USERS.remove(player.getUniqueId(), user);
+	}
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onKicked (PlayerKickEvent e)
+	{
+		Player player = e.getPlayer();
+		User user = Rebug.getUser(player);
+		if (user != null)
+			Rebug.USERS.remove(player.getUniqueId(), user);
+	}
 }
