8bc9c57f6f3efb391a1b185c1ea368347b4a2346
killstorm <38782331+killstorm103@users.noreply.github.com>
2024-05-01 21:06:49 +0800

    Update Test.java

--- me/killstorm103/Rebug/Commands/Test.java
+++ me/killstorm103/Rebug/Commands/Test.java
@@ -1,5 +1,7 @@
 package me.killstorm103.Rebug.Commands;
 
+import java.util.List;
+
 import org.bukkit.command.CommandSender;
 
 import me.killstorm103.Rebug.Main.Command;
@@ -28,9 +30,35 @@ public class Test extends Command
 		return StartOfPermission() + "test";
 	}
 	@Override
-	public void onCommand(CommandSender sender, String[] args) throws Exception
+	public void onCommand(CommandSender sender, String command, String[] args) throws Exception
 	{
 		Packets =! Packets;
 		sender.sendMessage("PacketEvents= " + Packets);
 	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, org.bukkit.command.Command command, String[] args) {
+		return null;
+	}
+
+	@Override
+	public boolean HasCustomTabComplete() {
+		return false;
+	}
+
+	@Override
+	public boolean HideFromCommandsList() {
+		return false;
+	}
+
+	@Override
+	public boolean HasToBeConsole() {
+		return false;
+	}
+
+	@Override
+	public String[] SubAliases() {
+		return null;
+	}
+
 }
