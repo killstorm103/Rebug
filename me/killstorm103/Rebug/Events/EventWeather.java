34f5c95f9f1ee1928af3c83360f4cf3f040ef7fa
killstorm <38782331+killstorm103@users.noreply.github.com>
2024-05-01 21:00:10 +0800

    Update EventWeather.java

--- me/killstorm103/Rebug/Events/EventWeather.java
+++ me/killstorm103/Rebug/Events/EventWeather.java
@@ -4,11 +4,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.weather.WeatherChangeEvent;
 
+import me.killstorm103.Rebug.Main.Config;
+
 public class EventWeather implements Listener
 {
 	@EventHandler
 	public void onWeather (WeatherChangeEvent e)
 	{
-		e.setCancelled(Rebug.getGetMain().getConfig().getBoolean("cancel-weather-changes"));
+		e.setCancelled(Config.ShouldCancelWeatherChanges());
 	}
 }
