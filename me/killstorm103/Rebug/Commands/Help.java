8d31660ca97d6280b746d141537f3dce6a13aaf0
killstorm <38782331+killstorm103@users.noreply.github.com>
2024-05-01 21:07:47 +0800

    Update Help.java

--- me/killstorm103/Rebug/Commands/Help.java
+++ me/killstorm103/Rebug/Commands/Help.java
@@ -1,5 +1,7 @@
 package me.killstorm103.Rebug.Commands;
 
+import java.util.List;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -35,7 +37,7 @@ public class Help extends Command
 	
 
 	@Override
-	public void onCommand(CommandSender sender, String[] args) throws Exception 
+	public void onCommand(CommandSender sender, String command, String[] args) throws Exception 
 	{
 		if (args.length == 1)
 			Log(sender, ChatColor.GRAY + "commands" + ChatColor.RESET + ":");
@@ -47,13 +49,46 @@ public class Help extends Command
 				if (sender instanceof Player)
 				{
 					if (((Player) sender).hasPermission(commands.getPermission()) || ((Player) sender).hasPermission(Rebug.AllCommands_Permission))
-						Log(sender, ChatColor.GRAY + "/rebug " + commands.getName());
+					{
+						if (!commands.HideFromCommandsList())
+							Log(sender, ChatColor.GRAY + "/rebug " + commands.getName());
+					}
 				}
 				else
-					Log(sender, ChatColor.GRAY + "/rebug " + commands.getName());
+				{
+					if (!commands.HideFromCommandsList())
+						Log(sender, ChatColor.GRAY + "/rebug " + commands.getName());
+				}
 			}
-			if (args.length == 3 && args[1].equalsIgnoreCase("command") && args[2].equalsIgnoreCase(commands.getName()))
+			if (args.length == 3 && (args[1].equalsIgnoreCase("cmd") || args[1].equalsIgnoreCase("command")) && args[2].equalsIgnoreCase(commands.getName()))
+			{
 				Log(sender, ChatColor.GRAY + "/rebug " + commands.getSyntax());
+				Log(sender, ChatColor.GRAY + commands.getDescription());
+			}
 		}
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, org.bukkit.command.Command command, String[] args) {
+		return null;
+	}
+
+	@Override
+	public boolean HasCustomTabComplete() {
+		return false;
+	}
+
+	@Override
+	public boolean HideFromCommandsList() {
+		return false;
+	}
+	@Override
+	public boolean HasToBeConsole() {
+		return false;
+	}
+
+	@Override
+	public String[] SubAliases() {
+		return null;
 	}
 }
