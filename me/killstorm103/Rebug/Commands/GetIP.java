1825d89144935e1022d39299a28e9a453a339caa
killstorm <38782331+killstorm103@users.noreply.github.com>
2024-05-01 21:08:16 +0800

    Update GetIP.java

--- me/killstorm103/Rebug/Commands/GetIP.java
+++ me/killstorm103/Rebug/Commands/GetIP.java
@@ -1,5 +1,8 @@
 package me.killstorm103.Rebug.Commands;
 
+import java.util.List;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -25,18 +28,48 @@ public class GetIP extends Command
 	}
 
 	@Override
-	public void onCommand(CommandSender sender, String[] args) throws Exception {
+	public void onCommand(CommandSender sender, String command, String[] args) throws Exception {
 		if (args.length < 2)
 		{
-			sender.sendMessage(getSyntax());
+			sender.sendMessage(Rebug.RebugMessage + getSyntax());
 			return;
 		}
-		Player player = Rebug.getGetMain().getServer().getPlayer(args[1]);
-		sender.sendMessage(sender.getName() + " " + player.getName() + "'s ip is: " + player.getAddress().getHostName() + ":" + player.getAddress().getPort());
+		Player player = Bukkit.getServer().getPlayer(args[1]);
+		if (player == null)
+		{
+			sender.sendMessage(Rebug.RebugMessage + " player was not found!");
+			return;
+		}
+		sender.sendMessage(Rebug.RebugMessage + player.getName() + "'s ip is: " + player.getAddress().getHostName() + ":" + player.getAddress().getPort());
 	}
 	@Override
 	public String getPermission() {
 		return "me.killstorm103.rebug.commands.getip";
 	}
-	
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, org.bukkit.command.Command command, String[] args) {
+		return null;
+	}
+
+	@Override
+	public boolean HasCustomTabComplete() {
+		return false;
+	}
+
+	@Override
+	public boolean HideFromCommandsList() {
+		return false;
+	}
+
+	@Override
+	public boolean HasToBeConsole() {
+		return false;
+	}
+
+	@Override
+	public String[] SubAliases() {
+		return null;
+	}
+
 }
