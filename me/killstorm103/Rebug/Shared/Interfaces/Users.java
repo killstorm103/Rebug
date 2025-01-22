136adb845f16cdcba93e69bd6a35f07befab6bc7
killstorm <38782331+killstorm103@users.noreply.github.com>
2025-01-22 22:21:28 +0800

    Delete me/killstorm103/Rebug directory

--- me/killstorm103/Rebug/Shared/Interfaces/Users.java
+++
@@ -1,6 +0,0 @@
-package me.killstorm103.Rebug.Shared.Interfaces;
-
-public interface Users
-{
-	
-}
--- me/killstorm103/Rebug/Shared/Server/ConsoleFilter/Engine.java
+++
@@ -1,8 +0,0 @@
-package me.killstorm103.Rebug.Shared.Server.ConsoleFilter;
-
-public interface Engine 
-{
-	public void HideConsoleMessages();
-    public int getHiddenMessagesCount();
-    public void AddHiddenMessage ();
-}
